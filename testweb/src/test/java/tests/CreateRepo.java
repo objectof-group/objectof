086f9c2b9b68411eaf86fcd2879f5ffca917cce8
Nathaniel Sherry <nsherry4@uwo.ca>
2015-07-08 16:51:39 -0400

    Removed code-gen plugin from testweb

--- testweb/src/test/java/tests/CreateRepo.java
+++ testweb/src/test/java/tests/CreateRepo.java
@@ -1,10 +1,12 @@
 package tests;
 
+
 import java.io.File;
 
 import net.objectof.model.Package;
 import net.objectof.repo.impl.rip.IRip;
 import net.objectof.repo.impl.sql.ISqlDb;
+
 
 /**
  * Creates a SqlRepo (a Rip is an instance of a SqlRepo; for creation purposes
@@ -13,15 +15,14 @@ import net.objectof.repo.impl.sql.ISqlDb;
  * @author jdh
  *
  */
-public class CreateRepo
-{
-  final static String repositoryName = "test.objectof.org:1407/test/person";
-  private static final String SCH = "../objectof.derived/src/main/resources/packages/test.xml";
+public class CreateRepo {
 
-  public static void main(String[] args)
-  {
-    ISqlDb db = new ISqlDb("testDatabase");
-    Package repo = db.create(repositoryName, IRip.class, new File(SCH));
-    System.out.println(repo.getUniqueName());
-  }
+    final static String repositoryName = "test.objectof.org:1407/test/person";
+    private static final String SCH = "src/main/resources/packages/test.xml";
+
+    public static void main(String[] args) {
+        ISqlDb db = new ISqlDb("testDatabase");
+        Package repo = db.create(repositoryName, IRip.class, new File(SCH));
+        System.out.println(repo.getUniqueName());
+    }
 }
